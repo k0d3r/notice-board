52e9e09e4f500081c1ee794980971b43ee8590c9
_ <_@_.com>
2019-10-06 22:13:28 +0100

    Commit

---
+++ sample-data.js
@@ -0,0 +1,57 @@
+const notices = [
+    {
+        id: 1,
+        title: 'Title One',
+        summary: 'Notice One Summary',
+        content: '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>',
+        date: 'Sept 4 2019, 08:08 PM',
+        author: 'John James',
+        comments: 3,
+        likes: 6,
+        dislikes: 1
+    },
+    {
+        id: 2,
+        title: 'Title Two',
+        summary: 'Notice Two Summary',
+        content: '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>',
+        date: 'Sept 9 2019, 10:17 AM',
+        author: 'Jack Jules',
+        comments: 0,
+        likes: 1,
+        dislikes: 2
+    },
+    {
+        id: 3,
+        title: 'Title Three With a Really Long Title',
+        summary: 'Notice Three Summary',
+        content: '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>',
+        date: 'Sept 16 2019, 07:03 PM',
+        author: 'John Johnson',
+        comments: 6,
+        likes: 4,
+        dislikes: 0
+    },
+    {
+        id: 4,
+        title: 'Title Four',
+        summary: 'Notice Four Summary',
+        content: '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>',
+        date: 'Sept 26 2019, 01:27 PM',
+        author: 'Joe James',
+        comments: 2,
+        likes: 7,
+        dislikes: 1
+    },
+    {
+        id: 3,
+        title: 'Title Five',
+        summary: 'Notice Five Summary',
+        content: '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>',
+        date: 'Sept 4 2019, 07:32 AM',
+        author: 'James Jackson',
+        comments: 7,
+        likes: 3,
+        dislikes: 2
+    }
+];
